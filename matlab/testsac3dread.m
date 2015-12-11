7003282b55335072fed3ffb9409422be17b5b5a2
mikeg64 <m.griffiths@sheffield.ac.uk>
2015-12-11 10:26:48 +0000

    testsca3dread loads data from sac fortran output correctly

--- matlab/testsac3dread.m
+++ matlab/testsac3dread.m
@@ -1,7 +1,7 @@
 %getpicttest  3D version
 % Read the npict-th picture from 1 or more files
 %http://uk.mathworks.com/matlabcentral/answers/97118-how-do-i-read-a-fortran-unformatted-binary-data-file-into-matlab
-filename='/fastdata/cs1mkg/sac/p5b0_0_bv10g_a1250/3D_spic_128_bv10g_13.out';
+filename='/fastdata/cs1mkg/sac/p5b0_0_bv10g_a1250/3D_spic_128_bv10g_12.out';
 
     fid=fopen(filename,'rb');
 
@@ -42,11 +42,13 @@ filename='/fastdata/cs1mkg/sac/p5b0_0_bv10g_a1250/3D_spic_128_bv10g_13.out';
    hr2=fread(fid, 1, 'int32');
    
    X=zeros(nxs,ndim);
-   w=zeros(nxs,nw);
-   for idim=1:ndim
-     hr1=fread(fid, 1, 'int32');
+   tw=zeros(nxs,13);
+       hr1=fread(fid, 1, 'int32');
      hr2=fread(fid, 1, 'int32');
+   for idim=1:ndim
+  
       X(:,idim)=fread(fid,nxs,'float64');
+      
    end
    
       nx1=nx(1);
@@ -57,35 +59,39 @@ filename='/fastdata/cs1mkg/sac/p5b0_0_bv10g_a1250/3D_spic_128_bv10g_13.out';
    yy=reshape(X(:,2),nx1,nx2,nx3);
    zz=reshape(X(:,3),nx1,nx2,nx3);
    
-     for iw=1:nw
-              hr1=fread(fid, 1, 'int32');
-     hr2=fread(fid, 1, 'int32');
-
-      %fread(fid,4);
-      w(:,iw)=fread(fid,nxs,'float64');
-      %fread(fid,4);
-   end 
+ 
+for iw1=1:13
+      
+      tbuf=fread(fid,nxs,'float64');
+       stb=size(tbuf);
+       if stb(1)<nxs
+         for itb=1:nxs-stb(1)
+           tbuf(stb(1)+itb)=0.0;
+        end
+       end
+      tw(:,iw1)=tbuf;
+end 
    
      % extract variables from w into variables named after the strings in wnames
 wd=zeros(nw,nx1,nx2,nx3);
 for iw=1:nw
   
-     tmp=reshape(w(:,iw),nx1,nx2,nx3);
+     tmp=reshape(tw(:,iw),nx1,nx2,nx3);
      wd(iw,:,:,:)=tmp;
 end
 
 
-%w=tmp(iw);
+
   
 
 clear tmp; 
    
    
    fclose(fid);
-   val=reshape(wd(1,:,1,:),128,128);
+   val=reshape(wd(1,:,64,:),128,128);
    surf(val,'LineStyle','none');
    colorbar;
    
-   val1=reshape(wd(3,:,1,:),128,128)/(reshape(wd(1,:,1,:),128,128)+reshape(wd(10,:,1,:),128,128));
-   val2=reshape(wd(4,:,1,:),128,128)/(reshape(wd(1,:,1,:),128,128)+reshape(wd(10,:,1,:),128,128));
+   val1=reshape(wd(3,:,64,:),128,128)./(reshape(wd(1,:,64,:),128,128)+reshape(wd(10,:,1,:),128,128));
+   val2=reshape(wd(4,:,64,:),128,128)./(reshape(wd(1,:,64,:),128,128)+reshape(wd(10,:,1,:),128,128));
    
